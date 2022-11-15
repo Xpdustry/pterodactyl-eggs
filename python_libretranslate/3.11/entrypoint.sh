e300cbbfbd378b483e127d4033dc30a4ecd806af
phinner <62483793+Phinner@users.noreply.github.com>
2022-11-15 22:44:08 +0100

    init

---
+++ python_libretranslate/3.11/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+cd /home/container
+
+# Make internal Docker IP address available to processes.
+INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
+export INTERNAL_IP
+
+# Print current Python version
+python --version
+
+# Replace Startup Variables
+MODIFIED_STARTUP=$(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+
+# apt install pkg-config libicu-dev
+# Run the Server
+eval ${MODIFIED_STARTUP}
