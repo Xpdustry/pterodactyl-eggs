64700aed6cd0ba12f4ab52b127d4aa306d3d6d25
phinner <62483793+Phinner@users.noreply.github.com>
2022-11-16 14:30:13 +0100

    proper libretranslate image

---
+++ libretranslate/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+cd /home/container
+
+# Make internal Docker IP address available to processes.
+INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
+export INTERNAL_IP
+
+# Print current Python version
+python --version
+
+# Replace Startup Variables
+MODIFIED_STARTUP=$(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+PATH="$PATH:./venv/bin"
+
+# apt install pkg-config libicu-dev
+# Run the Server
+eval ${MODIFIED_STARTUP}
